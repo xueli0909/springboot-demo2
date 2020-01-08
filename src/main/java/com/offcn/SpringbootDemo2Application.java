97a7c2daefd63c40aa97903b7f604f288fdc9344
king <as429005622@qq.com>
2020-01-08 20:07:22 +0800

    新增注释测试一下

--- src/main/java/com/offcn/SpringbootDemo2Application.java
+++ src/main/java/com/offcn/SpringbootDemo2Application.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootDemo2Application {
 
+    /**
+     * 新增注释测试一下
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemo2Application.class, args);
     }
